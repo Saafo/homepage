1d9872c05ab3efe95221defc6bde194cfc871f60
Saafo <saffordh@163.com>
2020-04-07 17:40:28 +0800

    feat: sidebar

--- js/index.js
+++ js/index.js
@@ -5,14 +5,21 @@ window.onload = function changeBackgroundColor() {
         '#2e77ff', //深蓝
         '#31e4eb', //蓝绿
         '#b6d333', //青绿
-        '#38e032', //浅绿
+        // '#38e032', //浅绿
         '#F6D656', //黄色
         '#F7695A', //浅红
-        '#f13636', //正红
+        // '#f13636', //正红
         '#a981ef', //浅紫
     ];
     var randomColor = colorCollections[Math.floor(Math.random()*colorCollections.length)];
-    this.document.getElementsByTagName('nav')[0].style.backgroundColor = randomColor;
-    this.document.getElementsByClassName('title-container')[0].style.backgroundColor = randomColor;
-    this.document.getElementsByTagName("h1")[0].style.color = randomColor;
+
+    randombgList = this.document.getElementsByClassName("random-bg-color");
+    for(let i = 0, len = randombgList.length; i < len; i++) {
+        randombgList[i].style.backgroundColor = randomColor;
+    }
+    randomList = this.document.getElementsByClassName("random-color");
+    for(let i = 0, len = randomList.length; i < len; i++) {
+        randomList[i].style.color = randomColor;
+    }
+
 }
