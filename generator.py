677295f0eddbdda45e5014401922b567d570de20
Saafo <saffordh@163.com>
2020-04-09 23:49:35 +0800

    feat: rss supported

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 import os
 import re
+import time
 import markdown
 
 class Post:
@@ -20,19 +21,19 @@ def getInfo():
     mdFiles.sort()
     mdFiles.reverse()
 
-    postList = []
+    blogList = []
 
     for i in range(len(mdFiles)):
-        postList.append(Post('', '', '', ''))
+        blogList.append(Post('', '', '', ''))
 
     content = ''
     blogAbstractsMD = []
 
     for i in range(len(mdFiles)):
-        postList[i].blogName = mdFiles[i][0:-3]
+        blogList[i].blogName = mdFiles[i][0:-3]
 
     for i in range(len(mdFiles)):
-        postList[i].blogTime = mdFiles[i][0:8]
+        blogList[i].blogTime = mdFiles[i][0:8]
 
     for i in range(len(mdFiles)):
         content = ''
@@ -42,18 +43,21 @@ def getInfo():
 
             rawTitle = re.search(r'# .*\n',content).group()
             title = rawTitle[2:-1]
-            postList[i].blogTitle = title
+            blogList[i].blogTitle = title
 
             rawAbstract = re.search(rawTitle+'[\s\S]{1,}',content).group()
             abstract = rawAbstract[len(rawTitle):]
             blogAbstractsMD.append(abstract)
     for i in range(len(blogAbstractsMD)): #将Abstract转化为html格式
-        postList[i].blogAbstract = markdown.markdown(blogAbstractsMD[i])
+        blogList[i].blogAbstract = markdown.markdown(blogAbstractsMD[i])
 
-    return postList
+    return blogList
 
 #写入html
-def writeHTML(postList):
+def writeHTML(blogList):
+    if(os.path.exists('index.html') == False):
+        print('不存在原始index.html文件，生成失败')
+        return
     #处理原始index.html
     with open('index.html', mode='r', encoding='utf8') as f:
         original = f.read()
@@ -64,9 +68,9 @@ def writeHTML(postList):
     tab = '    '#方便处理缩进
     space28 = '                            '#方便处理缩进
     newMainContent = ''
-    for i in range(len(postList)): 
-        blogLink = 'http://blog.mintsky.xyz/'+postList[i].blogName
-        newMainContent += "                <div class='post'>\n                    <header class='post-header'>\n                        <h1 class='post-title'>\n                            <a href='{blogLink}' class='random-color'>\n                                {blogTitle}\n                            </a>\n                        </h1>\n                        <div class='post-meta'>\n                            <span class='post-time'>{blogYear}·{blogMonth}·{blogDay}</span>\n                        </div>\n                    </header>\n                    <div class='post-body markdown-body'>\n                        {blogAbstract}                    </div>\n                    <div class='post-button random-bg-color'>\n                        <a href='{blogLink}'>\n                            阅读全文\n                        </a>\n                    </div>\n                </div>\n".format(blogLink=blogLink, blogTitle=postList[i].blogTitle, blogYear=postList[i].blogTime[0:4], blogMonth=postList[i].blogTime[4:6], blogDay=postList[i].blogTime[6:8],blogAbstract=re.sub('\n','\n'+space28, postList[i].blogAbstract)) #space28:处理缩进
+    for i in range(len(blogList)): 
+        blogLink = 'http://blog.mintsky.xyz/'+blogList[i].blogName
+        newMainContent += "                <div class='post'>\n                    <header class='post-header'>\n                        <h1 class='post-title'>\n                            <a href='{blogLink}' class='random-color'>\n                                {blogTitle}\n                            </a>\n                        </h1>\n                        <div class='post-meta'>\n                            <span class='post-time'>{blogYear}·{blogMonth}·{blogDay}</span>\n                        </div>\n                    </header>\n                    <div class='post-body markdown-body'>\n                        {blogAbstract}                    </div>\n                    <div class='post-button random-bg-color'>\n                        <a href='{blogLink}'>\n                            阅读全文\n                        </a>\n                    </div>\n                </div>\n".format(blogLink=blogLink, blogTitle=blogList[i].blogTitle, blogYear=blogList[i].blogTime[0:4], blogMonth=blogList[i].blogTime[4:6], blogDay=blogList[i].blogTime[6:8],blogAbstract=re.sub('\n','\n'+space28, blogList[i].blogAbstract)) #space28:处理缩进
 
     #备份
     os.rename('index.html', 'index.html.old')
@@ -78,14 +82,35 @@ def writeHTML(postList):
         f.write('            </div>\n        </div>\n        <aside')
         f.write(part2)
 
-def writeRSS(postList): #TODORSS
-    pass 
+def timeFormat(blogTime):
+    return time.strftime('%a, %d %b %Y %H:%M:%S GMT',time.strptime(blogTime,'%Y%m%d'))
+
+def writeRSS(blogList):
+    part1 = "<?xml version='1.0' encoding='utf-8'?>\n<rss version='2.0'>\n    <channel>\n        <title>Mintsky's Blog</title>\n        <link>http://mintsky.xyz/rss.xml</link>\n        <description>Posts' Abstracts on Mintsky's Blog</description>\n        <lastBuildDate>{buildTime}</lastBuildDate>\n        <language>zh-CN</language>\n        <generator>https://github.com/saafo/homepage/blob/master/generator.py</generator>\n        <copyright>Copyright 2020 Mintsky All Rights Reserved</copyright>\n\n".format(buildTime=timeFormat(blogList[0].blogTime))
+    part2 = "\n    </channel>\n</rss>"
+
+    #构造新main-content
+    space16 = "                "
+    newMainContent = ''
+    for i in range(len(blogList)):
+        blogLink = 'http://blog.mintsky.xyz/'+blogList[i].blogName
+        newMainContent += "        <item>\n            <title>{blogTitle}</title>\n            <link>{blogLink}</link>\n            <guid>{blogLink}</guid>\n            <pubDate>{blogTime}</pubDate>\n            <description>\n                <![CDATA[{blogAbstract}]]>\n            </description>\n        </item>\n".format(blogLink=blogLink, blogTitle=blogList[i].blogTitle, blogTime=timeFormat(blogList[i].blogTime),blogAbstract=re.sub('\n','\n'+space16, blogList[i].blogAbstract + "\n<p>阅读更多...</p>\n"))
+    
+    #备份
+    if(os.path.exists('rss.xml')):
+        os.rename('rss.xml', 'rss.xml.old')
+    #合成新html
+    with open('rss.xml',mode='w',encoding='utf8') as f:
+        f.write(part1)
+        f.write(newMainContent)
+        f.write(part2)
+
 
 def main():
-    postList = []
-    postList = getInfo()
-    writeHTML(postList)
-    writeRSS(postList)
+    blogList = []
+    blogList = getInfo()
+    writeHTML(blogList)
+    writeRSS(blogList)
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -1,2 +1,51 @@
-# My homepage
-[Link](http://mintsky.xyz)
+# My Homepage
+
+[Mintsky's Homepage](http://mintsky.xyz)
+
+>人类的本质是造轮子。
+
+我的博客和随之诞生的辣鸡博客模板
+
+## 技术架构
+
+### 依赖：
+
+* [jQuery](https://jquery.com)
+* [marked.js](https://marked.js.org/)
+* [hightlight.js](http://highlightjs.org/)
+* [github-markdown-css](https://sindresorhus.com/github-markdown-css/)
+* [Python-Markdown](https://python-markdown.github.io/)
+* [utterances](https://utteranc.es/)
+
+### 结构：
+
+* `index.html`和`rss.xml`为`generator.py`根据原始`index.html`和`/blog`文件夹下的`markdown`文件生成的文件。每次添加新博客后需要手动运行。
+* `blog-template.html`为所有博客页面的框架页面，客户端通过读取服务器上的`markdown博客文件`和`blog-template.html`实时渲染出博客页面。
+* `/blog文件夹`存放博客。
+* 评论系统由[utterances](https://utteranc.es/)提供。
+
+## 使用方法
+
+* clone this repository
+* 全文搜索`mintsky.xyz`替换为您的域名，并将有必要修改的地方都修改掉。这是基于我的需求创建的仓库，所以没有做分离，鲁棒性不好。
+* pip install markdown
+* `nginx`配置：
+  ```
+  server{
+                listen 80;
+                server_name blog.mintsky.xyz;
+                location = / {
+                        return 301 http://mintsky.xyz;
+                }
+                location / {
+                        root (这里放homepage文件夹目录);
+                        try_files $uri /blog-template.html =404;
+                }
+        }
+  ```
+* 每次更新博客时将`markdown文件`放入`/blog文件夹`，然后运行`generator.py`。
+
+## 注意事项
+
+* 与`Github Pages`不兼容。如果要在其上使用需要修改js文件。
+* `utterances`使用第三方cookies导致[401报错的问题](https://github.com/utterance/utterances/issues/123)
